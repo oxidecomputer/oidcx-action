1de4de3cf8438126fc9f006fc355904ce087381e
augustuswm <gusmayo@gmail.com>
2026-03-30 14:54:21 -0500

    More log lines

--- src/index.js
+++ src/index.js
@@ -72,8 +72,11 @@ const configureGitCredentials = async (service, accessToken) => {
     core.info(`While unsetting http.${host}.extraheader git returned error. Checking error code to determine if credentials were unset.`)
     core.info(err.toString())
 
+    const isStatus5Error = err.toString().endsWith("failed with exit code 5")
+    core.info(`Test for status code 5 error: ${isStatus5Error}`)
+
     // We want to ignore status code 5 which will be returned if there are no credentials to unset.
-    if (err.toString().endsWith("failed with exit code 5")) {
+    if (isStatus5Error) {
       core.info(`While unsetting http.${host}.extraheader git returned status code 5, no credentials to unset`)
       return 0
     }
